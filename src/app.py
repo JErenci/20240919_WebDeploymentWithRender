1013be4ec8c680997aa1c0826090f9854a931981
Joan Guerrero <joan.guerrero.erencia@gmail.com>
2024-09-19 09:00:32 +0200

    Update app.py
    wit date and time

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from dash import Dash, html
 app = Dash()
 server = app.server
 
-app.layout = [html.Div(children='Hello World')]
+app.layout = [html.Div(children='Hello World 19 Sept 2024 9h in Sant JoanDespí')]
 
 if __name__ == '__main__':
     app.run(debug=True)
