c7fe0190e2d308cb8db6c84bc112ae57198d5229
JGuerrero <joan.guerrero.erencia@gmail.com>
2024-09-20 06:21:41 +0200

    Paste drag and drop functionality

--- src/app.py
+++ src/app.py
@@ -1,9 +1,93 @@
-from dash import Dash, html
+from dash import Dash, dcc, html, Input, Output, State, callback, dash_table
+import pandas as pd
+import base64
+import datetime
+import io
 
-app = Dash()
-server = app.server
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = [html.Div(children='Hello World')]
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-image-upload'),
+])
+
+def parse_contents(contents, filename, date):
+
+    if ('text' in contents):
+        print('text')
+        content_type, content_string = contents.split(',')
+        decoded = base64.b64decode(content_string)
+        print('decoded:',decoded)
+        try:
+            if 'csv' in filename:
+                print('csv')
+                # Assume that the user uploaded a CSV file
+                df = pd.read_csv(
+                    io.StringIO(decoded.decode('utf-8')))
+            elif 'xls' in filename:
+                # Assume that the user uploaded an excel file
+                df = pd.read_excel(io.BytesIO(decoded))
+        except Exception as e:
+            print(e)
+            return html.Div([
+                'There was an error processing this file.'
+            ])
+        im = dash_table.DataTable(df.to_dict('records'), page_size=10)
+    else:
+        print('img')
+        im = html.Img(src=contents)
+
+
+    div = [
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        # HTML images accept base64 encoded strings in the same format
+        # that is supplied by the upload
+        im,
+        html.Hr(),
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        }),
+    ]
+    return html.Div(div)
+
+@callback(Output('output-image-upload', 'children'),
+          # Output('tbl', 'data'),
+          Input('upload-image', 'contents'),
+          State('upload-image', 'filename'),
+          State('upload-image', 'last_modified'),
+          prevent_initial_callback=True)
+
+def update_output(list_of_contents,list_of_names, list_of_dates):
+
+    if list_of_contents is not None:
+
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
 if __name__ == '__main__':
     app.run(debug=True)
